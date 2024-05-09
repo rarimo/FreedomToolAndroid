f4979e4f41bfe2885a1fb3b8ff7f1f791cbb1ece
yivanov <apereliez1@gmail.com>
2024-05-09 18:02:28 +0300

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.freedomtool"
         minSdk = 27
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.2.1"
+        versionCode = 11
+        versionName = "1.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

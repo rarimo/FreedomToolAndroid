5bfd0bbcd52126f247d146ae896539fcf1ee8e4a
yivanov <apereliez1@gmail.com>
2024-05-18 12:11:33 +0300

    update Identity lib, update text, update settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.freedomtool"
         minSdk = 27
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.3.6"
+        versionCode = 17
+        versionName = "1.3.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
